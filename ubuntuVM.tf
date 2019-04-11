2277abb2589d948bbc46a09e61438d0bef0dcaeb
José Joaquín Cañadas Martínez <jjcanada@ual.es>
2019-04-11 18:47:39 +0200

    creacion maquina ansible-dev ok

--- ubuntuVM.tf
+++ ubuntuVM.tf
@@ -5,7 +5,7 @@ variable "user_name" {default = "jjcanada"}
 ## 2. Abrir el archivo token.json generado en la operación curl y copiar el token de acceso
 
 variable "my_token" {
-  default = "PASTE_YOUR_TOKEN_HERE"  
+  default = "gAAAAABcr2g3WfCzxtxpwlBtCu4DigqegVm40rXg_l-og_9gLD4RBwImR_--zx5MbsZw7rUJ9fXq2k6V71CP5ro16KRJBrDFhpxduSMhEed4vYenEeDZBW9i7kJykkTqf4sPUK8EjcUjag76EbQHbsPaiFJkQhKzgQkF32juVXb5X5ZJRmxrQlw"  
 }
 
 variable "project_name" {
@@ -27,19 +27,12 @@ provider "openstack" {
 }
 
 
-resource "tls_private_key" "deployAnsible" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "openstack"
-
 ########################################
 # Crear la Virtual Machine
 ########################################
 
 resource "openstack_compute_instance_v2" "mi_tf_instance" {
-  name      = "ub18vm-cnsa"
+  name      = "ansible-dev"
   availability_zone = "nova"
   image_id  = "9eabea01-c377-4911-9ee0-7276ae4ca820"    ## Ubuntu 18
   flavor_name = "medium"
@@ -59,126 +52,40 @@ resource "openstack_networking_floatingip_v2" "myip" {
 }
 
 # Asociar la IP
-resource "openstack_compute_floatingip_associate_v2" "myip" {
+resource "openstack_compute_floatingip_associate_v2" "myip_as" {
   floating_ip = "${openstack_networking_floatingip_v2.myip.address}"
   instance_id = "${openstack_compute_instance_v2.mi_tf_instance.id}"
-}
 
-########################################
-# Crear la Virtual Machine: node1
-########################################
-
-resource "openstack_compute_instance_v2" "mi_tf_node1" {
-  name      = "ub18node1-cnsa"
-  availability_zone = "nova"
-  image_id  = "9eabea01-c377-4911-9ee0-7276ae4ca820"    ## Ubuntu 18
-  flavor_name = "medium"
-  key_pair  = "${var.user_name}"  ## comprobar el nombre el Key Pair en OpenStack y cambiar si es necesario
-  security_groups = ["default"]
-  
-  network {
-    name = "${var.project_network}"
-  }
-
-}
-
-# Crear una IP de mi pool ext-net
-
-resource "openstack_networking_floatingip_v2" "myipnode1" {
-  pool = "ext-net"
-}
-
-# Asociar la IP
-resource "openstack_compute_floatingip_associate_v2" "myipnode1" {
-  floating_ip = "${openstack_networking_floatingip_v2.myipnode1.address}"
-  instance_id = "${openstack_compute_instance_v2.mi_tf_node1.id}"
-
-   provisioner "file" {
+  provisioner "file" {
     connection {
       type     = "ssh"
       user     = "ubuntu"
       private_key  = "${file("c:/users/joaquin/.ssh/id_rsa")}"
-      host = "${azurerm_public_ip.myterraformpublicip.address}"
+      host = "${openstack_networking_floatingip_v2.myip.address}"
     }
 
-    source      = "c:/DDatos msi/ansible/ansible_ssh_keypair/id_rsa.pub"
-    destination = "/home/ubuntu/.ssh/id_rsa_ansible.pub"
-  }
-
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key  = "${file("c:/users/joaquin/.ssh/id_rsa")}"
-      host = "${openstack_networking_floatingip_v2.myipnode1.address}"
-    }
-    inline = [
-       "sudo apt-get update -y",
-       "sudo apt-get upgrade -y",
-       "sudo apt-get install -y python-minimal",
-       "sudo reboot -h now"       
-      ]
-    on_failure = "continue"
-  }
-}
-
-########################################
-# Crear la Virtual Machine: node2
-########################################
-
-resource "openstack_compute_instance_v2" "mi_tf_node2" {
-  name      = "ub18node2-cnsa"
-  availability_zone = "nova"
-  image_id  = "9eabea01-c377-4911-9ee0-7276ae4ca820"    ## Ubuntu 18
-  flavor_name = "medium"
-  key_pair  = "${var.user_name}"
-  security_groups = ["default"]
-  
-  network {
-    name = "${var.project_network}"
-  }
-
-}
-
-# Crear una IP de mi pool ext-net
-
-resource "openstack_networking_floatingip_v2" "myipnode2" {
-  pool = "ext-net"
-}
-
-# Asociar la IP
-resource "openstack_compute_floatingip_associate_v2" "myipnode2" {
-  floating_ip = "${openstack_networking_floatingip_v2.myipnode2.address}"
-  instance_id = "${openstack_compute_instance_v2.mi_tf_node2.id}"
-  
-   provisioner "file" {
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key  = "${file("c:/users/joaquin/.ssh/id_rsa")}"
-      host = "${azurerm_public_ip.myterraformpublicip.address}"
-    }
-
-    source      = "c:/DDatos msi/ansible/ansible_ssh_keypair/id_rsa.pub"
-    destination = "/home/ubuntu/.ssh/id_rsa_ansible.pub"
+    // En una carpeta local tengo creado un par de claves (especifico) para ansible.
+    source      = "c:/DDatos msi/ansible/ansible_ssh_keypair/"    ## copia todos los archivos de la carpeta
+    destination = "/home/ubuntu/.ssh"
   }
   provisioner "remote-exec" {
     connection {
       type     = "ssh"
       user     = "ubuntu"
       private_key  = "${file("c:/users/joaquin/.ssh/id_rsa")}"
-      host = "${openstack_networking_floatingip_v2.myipnode2.address}"
+      host = "${openstack_networking_floatingip_v2.myip.address}"
     }
     inline = [
-       "sudo apt-get update -y",
-       "sudo apt-get upgrade -y",
-       "sudo apt-get install -y python-minimal",
-       "sudo more /home/ubuntu/.ssh/id_rsa_ansible.pub >> /home/ubuntu/.ssh/authorized_keys",
-       "sudo reboot -h now"       
+      "sudo apt-get update -y",
+ ##     "sudo apt-get upgrade -y",
+      "sudo apt-get install -y python-minimal",
+      "sudo more /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys",  ## inyecto la clave publica en authorized_keys
+      "sudo apt-get install -y software-properties-common", 
+      "sudo apt-add-repository --yes --update ppa:ansible/ansible", 
+      "sudo apt-get install -y ansible"
       ]
     on_failure = "continue"
   }
 
 }
-
 
